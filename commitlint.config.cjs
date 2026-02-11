555d93e629b404c09aeeb751e72ba7fab6fd39e2
russellbomer <russellbomer@gmail.com>
2026-02-11 06:00:49 +0000

    commitlint edit

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,9 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-empty": [0],
+    "subject-empty": [0],
+    "subject-case": [0],
+    "header-max-length": [0],
+  },
 };
